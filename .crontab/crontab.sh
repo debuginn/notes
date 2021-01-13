184ecbe2299cceb091bbd93f0c3b66946da63bb0
debuginn <debuginn@icloud.com>
2021-01-13 19:48:50 +0800

    crontab auto save

--- .crontab/crontab.sh
+++
@@ -1,2 +0,0 @@
-* * * * * /Users/mengxiaoyu/Documents/Notes/.crontab/autoSave.sh > /Users/mengxiaoyu/Documents/Notes/.crontab/autoSave.log 2>&1 &
-
--- autoSave.sh
+++ autoSave.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /Users/mengxiaoyu/Documents/Notes
+git config --local user.name 'debuginn' && git config --global user.email 'debuginn@icloud.com'
 git add .
 git commit -m "crontab auto save"
 git push -u origin main
