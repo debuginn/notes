12140e7e3fa370ee1b25ce97dee86db235a8d30f
mengxianhui <mengxianhui@xiaomi.com>
2021-01-13 09:24:35 +0800

    crontab auto save

---
+++ .crontab/autoSave.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /Users/mengxiaoyu/Documents/Notes
+git add .
+git commit -m "crontab auto save"
+git push -u origin main
---
+++ .crontab/crontab.sh
@@ -0,0 +1,2 @@
+* * * * * /Users/mengxiaoyu/Documents/Notes/.crontab/autoSave.sh > /Users/mengxiaoyu/Documents/Notes/.crontab/autoSave.log 2>&1 &
+
