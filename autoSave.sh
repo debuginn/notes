4b6854c5a753dd85d3a062add15dad44484723fc
debuginn <debuginn@icloud.com>
2021-01-30 17:13:28 +0800

    crontab auto save

--- autoSave.sh
+++ autoSave.sh
@@ -3,9 +3,9 @@ echo "------ job start ------"
 cd /Users/mengxiaoyu/Documents/Notes
 echo "- set local git config -"
 git config --local user.name 'debuginn' && git config --local user.email 'debuginn@icloud.com'
-echo "- git pull origin/main -"
-git pull
-echo "- git push origin/main -"
+echo "- git pull origin main -"
+git pull origin main
+echo "- git push origin main -"
 git add .
 git commit -m "crontab auto save"
 git push -u origin main
